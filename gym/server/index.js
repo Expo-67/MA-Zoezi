d2ae3125355301f7241dcec5ba0a0d05e12b16b9
Expo-67 <145459052+Expo-67@users.noreply.github.com>
2024-09-29 18:23:04 +0300

    commit day 32

--- gym/server/index.js
+++ gym/server/index.js
@@ -1,11 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
 
+import { connectDB } from "./DB/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hey guys its me!!!");
-});
+app.use("/api/auth", authRoutes);
 
-app.listen(3001, () => {
-  console.log("server is running in port 3001");
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server is running on port:", PORT);
 });
