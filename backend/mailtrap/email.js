683f262491af7cf0ff83e629e1f3a8f6c262456f
Expo-67 <145459052+Expo-67@users.noreply.github.com>
2024-10-08 05:02:24 +0300

    day 35 commit

--- backend/mailtrap/email.js
+++ backend/mailtrap/email.js
@@ -1,5 +1,6 @@
 import { mailtrapClient, sender } from "./mailtrap.config.js";
 import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplate.js";
+import { response } from "express";
 
 export const sendVerificationEmail = async (email, verificationToken) => {
   const recipient = [{ email }];
@@ -19,5 +20,26 @@ export const sendVerificationEmail = async (email, verificationToken) => {
   } catch (error) {
     console.error(`Error sending verification `, error);
     throw new Error(`Error sending verification: ${error}`);
+  }
+};
+
+export const sendWelcomeEmail = async (email, name) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: "1816009f-c639-44a2-aca7-a09e52409fd9",
+      template_variables: {
+        name: name,
+      },
+    });
+
+    console.log("Welcome email sent successfully", response);
+  } catch (error) {
+    console.error(`Error sending welcome email`, error);
+
+    throw new Error(`Error sending welcome email: ${error}`);
   }
 };
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -7,4 +7,5 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
+router.post("/verify-email", verifyEmail);
 export default router;
